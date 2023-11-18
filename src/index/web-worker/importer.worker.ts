781c2c4322c8eb982a41ef6756458a6c8ef11a68
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <i.am.the.tablet@proton.me>
2023-11-17 23:39:35 -0500

    change workers to deal with pdfs

--- src/index/web-worker/importer.worker.ts
+++ src/index/web-worker/importer.worker.ts
@@ -1,6 +1,8 @@
 import { markdownImport } from "index/import/markdown";
-import { ImportCommand, MarkdownImportResult } from "index/web-worker/message";
+import { pdfImport } from "index/import/pdf";
+import { ImportCommand, MarkdownImportResult, PdfImportResult } from "index/web-worker/message";
 import { Transferable } from "index/web-worker/transferable";
+import { TFile } from "obsidian";
 
 /** Web worker entry point for importing. */
 onmessage = (event) => {
@@ -16,6 +18,20 @@ onmessage = (event) => {
                     result: markdown,
                 } as MarkdownImportResult)
             );
+        } else if (message.type === "pdf") {
+            window.pdfjsLib?.getDocument(
+                app.vault.getResourcePath(app.vault.getAbstractFileByPath(message.path) as TFile)
+            )?.promise.then(pdf => {
+                if (pdf) {
+                    postMessage(
+                        Transferable.transferable({
+                            type: "pdf",
+                            result: pdfImport(message.path, message.stat, pdf),
+                        } as PdfImportResult)
+                    );
+                }
+            })
+            
         } else {
             postMessage({ $error: "Unsupported import method." });
         }
