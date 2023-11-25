d51951dc5d75a79104b85bb41d58cf42fe89daf4
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <i.am.the.tablet@proton.me>
2023-11-24 19:27:22 -0500

    move pdf import shenanigans elsewhere
    (cherry picked from commit 4203832e2cef94f34f38c47a021cf3ccb99ed313)

--- src/index/web-worker/importer.worker.ts
+++ src/index/web-worker/importer.worker.ts
@@ -19,46 +19,10 @@ onmessage = async (event) => {
                 } as MarkdownImportResult)
             );
         } else if (message.type === "pdf") {
-            /** dear reader, i know there is no good explanation for any of the following code... */
-
-            /** we need to shit-fill window.location to placate the pdf worker */
-            globalThis["window"] = {
-                // @ts-ignore
-                location: "app://obsidian.md",
-            };
-
-            /** add `document` shitfill to global object (to placate the pdf worker) */
-            Object.assign(globalThis, { document });
-            const rawPdfWorker = `data:text/javascript;base64,${btoa(
-                unescape(
-                    encodeURIComponent(
-                        await (
-                            await fetch("https://cdn.jsdelivr.net/npm/pdfjs-dist@latest/build/pdf.worker.mjs")
-                        ).text()
-                    )
-                )
-            )}`;
-            const rawPdfJs = `data:text/javascript;base64,${btoa(
-                unescape(
-                    encodeURIComponent(
-                        await (
-                            await fetch("https://cdn.jsdelivr.net/npm/pdfjs-dist@latest/build/pdf.min.mjs")
-                        ).text()
-                    )
-                )
-            )}`;
-            let pdfjsLib = await import(rawPdfJs)
-            pdfjsLib.GlobalWorkerOptions.workerSrc = rawPdfWorker;
-            console.debug(message.path, message.resourceURI);
-            pdfjsLib.GlobalWorkerOptions.workerPort = new Worker(rawPdfWorker, { type: "module" });
-
-            let { promise } = await pdfjsLib.getDocument(message.resourceURI);
-            let pdf = await promise;
-
             postMessage(
                 Transferable.transferable({
                     type: "pdf",
-                    result: pdfImport(message.path, message.stat, pdf),
+                    result: await pdfImport(message),
                 } as PdfImportResult)
             );
         } else {
