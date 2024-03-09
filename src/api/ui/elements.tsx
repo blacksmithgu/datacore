8126c163b7e82b6fd15e02f8daaf3ba461298dbf
Michael Brenan <michael@brenan.dev>
2024-03-09 00:22:03 -0800

    Add stack and group layout elements

--- src/api/ui/elements.tsx
+++
@@ -1,6 +0,0 @@
-/**
- * Collection of direct API-facing elements. This essentially serves as a layer of indirection to internal types, so that the internal types can be
- * changed without breaking the API.
- */
-
-import "+styles/test.css";
---
+++ src/api/ui/layout.tsx
@@ -0,0 +1,61 @@
+/**
+ * Collection of direct API-facing elements. This essentially serves as a layer of indirection to internal types, so that the internal types can be
+ * changed without breaking the API.
+ */
+
+import { h } from "preact";
+import { HTMLAttributes, useMemo } from "preact/compat";
+
+import "api/styles/layout.css";
+
+/** Creates a vertical flexbox "stack" of elements. */
+export function Stack(props: HTMLAttributes<HTMLDivElement> & {
+    className?: string;
+    justify?: string;
+    align?: string;
+    style?: string;
+}) {
+    const { className, justify, align, style, children, ...rest } = props;
+    const extraStyle = useMemo(() => {
+        let style = "";
+        if (justify) style += `justify-content: ${justify};`;
+        if (align) style += `align-items: ${align};`;
+
+        return style.length == 0 ? undefined : style;
+    }, [justify, align]);
+
+    return (
+        <div
+            className={className ? className + " dc-stack" : "dc-stack"}
+            style={style ? extraStyle + style : extraStyle}
+            {...rest}>
+            {children}
+        </div>
+    );
+}
+
+/** Creates a horizontal flexbox "grouping" of elements. */
+export function Group(props: HTMLAttributes<HTMLDivElement> & {
+    className?: string;
+    justify?: string;
+    align?: string;
+    style?: string;
+}) {
+    const { className, justify, align, style, children, ...rest } = props;
+    const extraStyle = useMemo(() => {
+        let style = "";
+        if (justify) style += `justify-content: ${justify};`;
+        if (align) style += `align-items: ${align};`;
+
+        return style.length == 0 ? undefined : style;
+    }, [justify, align]);
+
+    return (
+        <div
+            className={className ? className + " dc-group" : "dc-group"}
+            style={style ? extraStyle + style : extraStyle}
+            {...rest}>
+            {children}
+        </div>
+    );
+}
