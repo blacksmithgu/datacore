c30b580ecbd76ed4ab66c979c6e19512e7e4851f
Michael Brenan <michael@brenan.dev>
2024-10-07 22:51:29 -0700

    Refactor and simplify a bit of logic

--- src/ui/view-page.tsx
+++ src/ui/view-page.tsx
@@ -1,22 +1,116 @@
-import { debounce, ItemView, Menu, ViewStateResult, WorkspaceLeaf } from "obsidian";
+import { debounce, ItemView, MarkdownRenderChild, Menu, ViewStateResult, WorkspaceLeaf } from "obsidian";
 import { ScriptLanguage } from "utils/javascript";
 import { DatacoreJSRenderer, ReactRenderer } from "./javascript";
 import { DatacoreLocalApi } from "api/local-api";
 import { DatacoreApi } from "api/api";
 import { createContext } from "preact";
 import { Group, Stack } from "api/ui/layout";
-import { useCallback, useContext, useEffect, useState } from "preact/hooks";
+import { useCallback, useContext, useMemo, useState } from "preact/hooks";
 import { Textbox, VanillaSelect } from "api/ui/basics";
-import { useIndexUpdates, useStableCallback } from "./hooks";
-import DatacorePlugin from "main";
-import { DATACORE_CONTEXT } from "./markdown";
-import AsyncSelect, { AsyncProps } from "react-select/async";
-import { GroupBase } from "react-select";
+import { useIndexUpdates } from "./hooks";
+import { DATACORE_CONTEXT, ErrorMessage } from "./markdown";
+import Select from "react-select";
 
-export const VIEW_TYPE_DATACORE = "datacore-query-view";
+/** Key for datacore JS query views. */
+export const VIEW_TYPE_DATACOREJS = "datacorejs-view";
 
+/** Stores the current Obsidian view object, so it can be manipulated from react. */
 const CUSTOM_VIEW_CONTEXT = createContext<DatacoreQueryView>(undefined!);
 
+/** Provides options for configuring a datacore view pane. */
+function DatacoreViewSettings() {
+    const view = useContext(CUSTOM_VIEW_CONTEXT) as DatacoreQueryView;
+    const setViewState = useMemo(() => debounce((state: Partial<DatacoreViewState>) => view.setState(state, { history: false }), 500), [view]);
+
+    const [localState, setLocalState] = useState(view.getState());
+    const setState = useCallback((state: Partial<DatacoreViewState>) => {
+        const finalState = { ...localState, ...state };
+        // Not debounced.
+        setLocalState(finalState);
+
+        // Debounced.
+        setViewState(finalState);
+    }, [localState, setLocalState, view]);
+
+    return (
+        <Stack align="stretch">
+            <button
+                className="clickable-icon"
+                style="align-self: start"
+                onClick={() => view.view("script")}
+            >
+                {BACK_BUTTON}
+            </button>
+            <Group justify="space-between" align="center">
+                <h6>View Title</h6>
+                <Textbox
+                    defaultValue={view.getState().title}
+                    onChange={(e) => setState({ title: e.currentTarget.value as string })}
+                />
+            </Group>
+            <Group justify="space-between" align="center">
+                <h6>View Type</h6>
+                <VanillaSelect
+                    defaultValue={view.getState().sourceType}
+                    options={LANGUAGE_OPTIONS}
+                    value={localState.sourceType}
+                    onValueChange={(s) => setState({ sourceType: s as ScriptLanguage })}
+                />
+            </Group>
+            <Group justify="space-between" align="center">
+                <h6>Script/View source</h6>
+                <textarea
+                    style={{ resize: "vertical", minWidth: "75%", fontFamily: "monospace" }}
+                    defaultValue={localState.script}
+                    value={localState.script}
+                    onChange={(e) => setState({ "script": e.currentTarget.value as string})}
+                />
+            </Group>
+            <Group justify="space-between" align="center">
+                <Stack>
+                    <h6>Current File</h6>
+                    <small>The path returned by functions like `useCurrentPath` in this view</small>
+                </Stack>
+                <div style={{ minWidth: "50%" }}>
+                    <CurrentFileSelector defaultValue={localState.currentFile} onChange={(v) => setState({ currentFile: v })} />
+                </div>
+            </Group>
+        </Stack>
+    );
+}
+
+/** React component for asynchronously showing the active set of current files to select one from. */
+function CurrentFileSelector({ defaultValue, onChange }: { defaultValue?: string; onChange: (value: string | undefined) => void }) {
+    const core = useContext(DATACORE_CONTEXT);
+    const revision = useIndexUpdates(core, { debounce: 2000 });
+
+    // Cached list of relevant files, which is only recomputed on vault changes.
+    const options = useMemo(() => {
+        return core.vault.getMarkdownFiles().map((f) => ({ label: f.path, value: f.path }));
+    }, [revision]);
+
+    const defaultOption = defaultValue ? { label: "No file", value: undefined} : { label: defaultValue, value: defaultValue };
+
+    return (
+        <Select
+            options={options}
+            classNamePrefix="datacore-selectable"
+            defaultValue={defaultOption}
+            onChange={(nv, _am) => onChange(nv?.value)}
+            unstyled
+        />
+    );
+}
+
+/** Selectable options for picking which language to execute the script in. */
+const LANGUAGE_OPTIONS: { label: string; value: ScriptLanguage }[] = [
+    { label: "Javascript", value: "js", },
+    { label: "Typescript", value: "ts", },
+    { label: "Javascript (JSX)", value: "jsx", },
+    { label: "Typescript JSX", value: "tsx", },
+];
+
+/** SVG back button shown to exit the settings view. */
 const BACK_BUTTON = (
     <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -35,195 +129,66 @@ const BACK_BUTTON = (
     </svg>
 );
 
-function useViewState<T>(): [T, (prop: keyof T, value: any) => void, () => void] {
-    const view = useContext(CUSTOM_VIEW_CONTEXT) as ItemView;
-    const [state, setter] = useState(view.getEphemeralState() as T);
-    const stateSaver = useCallback(() => {
-        view.setState(state, { history: false });
-    }, [state, setter]);
-    const setState = (prop: keyof T, value: any) => {
-        setter((v) => ({ ...v, [prop]: value }));
-    };
-    return [state, setState, stateSaver];
-}
-
-function DatacoreViewSettings() {
-    const core = useContext(DATACORE_CONTEXT);
-    let revision = useIndexUpdates(core);
-    const [internalState, setInternalState, saveState] = useViewState<DatacoreViewState>();
-    const options = [
-        {
-            label: "Javascript",
-            value: "js",
-        },
-        {
-            label: "Typescript",
-            value: "ts",
-        },
-        {
-            label: "Javascript (JSX)",
-            value: "jsx",
-        },
-        {
-            label: "Typescript JSX",
-            value: "tsx",
-        },
-    ];
-    const debouncedSave = debounce(saveState, 250, true);
-    useEffect(() => {
-        debouncedSave();
-    }, [internalState]);
-    const view = useContext(CUSTOM_VIEW_CONTEXT);
-    const debouncedFetch = debounce(
-        useStableCallback(
-            (input: string, callback: (options: { label: string; value: string }[]) => void) => {
-                callback(
-                    core.vault
-                        .getMarkdownFiles()
-                        .filter((x) => x.path.toLocaleLowerCase().includes(input.toLocaleLowerCase()))
-                        .map((f) => ({ label: f.path, value: f.path }))
-                );
-            },
-            [revision]
-        ),
-        300
-    );
-    return (
-        <Stack align="stretch">
-            <button
-                className="clickable-icon"
-                style="align-self: start"
-                onClick={() => {
-                    saveState();
-                    view.toggleSettings(false);
-                }}
-            >
-                {BACK_BUTTON}
-            </button>
-            <Group justify="space-between" align="center">
-                <h6>View Title</h6>
-                <Textbox
-                    defaultValue={view.getState().title}
-                    onChange={(e) => setInternalState("title", e.currentTarget.value as string)}
-                />
-            </Group>
-            <Group justify="space-between" align="center">
-                <h6>View Type</h6>
-                <VanillaSelect
-                    defaultValue={view.getState().sourceType}
-                    options={options}
-                    value={internalState.sourceType}
-                    onValueChange={(s) => setInternalState("sourceType", s)}
-                />
-            </Group>
-            <Group justify="space-between" align="center">
-                <h6>Script/View source</h6>
-                <textarea
-                    style={{ resize: "vertical", minWidth: "75%", fontFamily: "monospace" }}
-                    defaultValue={view.getState().script}
-                    value={internalState.script}
-                    onChange={(e) => setInternalState("script", e.currentTarget.value as string)}
-                />
-            </Group>
-            <Group justify="space-between" align="center">
-                <Stack>
-                    <h6>Current File</h6>
-                    <small>The path returned by functions like `useCurrentPath` in this view</small>
-                </Stack>
-                <div style={{minWidth: "50%"}}>
-                    <AsyncSelect
-                        loadOptions={
-                            debouncedFetch as unknown as AsyncProps<
-                                { value: string; label: string },
-                                false,
-                                GroupBase<{ value: string; label: string }>
-                            >["loadOptions"]
-                        }
-                        menuPortalTarget={document.body}
-                        classNames={{
-                            input: (props: any) => "prompt-input",
-                            valueContainer: (props: any) => "suggestion-item value-container",
-                            container: (props: any) => "suggestion-container",
-                            menu: (props: any) => "suggestion-content suggestion-container",
-                            option: (props: any) => `suggestion-item${props.isSelected ? " is-selected" : ""}`,
-                        }}
-                        classNamePrefix="datacore-selectable"
-                        cacheOptions
-												onChange={(nv, am) => {
-													setInternalState("currentFile", nv?.value)
-												}}
-                        unstyled
-												defaultValue={{value: view.getState().currentFile, label: view.getState().currentFile}}
-                        defaultOptions={[{ value: view.getState().currentFile, label: view.getState().currentFile }]}
-                    />
-                </div>
-            </Group>
-        </Stack>
-    );
-}
-
+/** State for the datacore view page. */
 export interface DatacoreViewState {
-    sourceType: /* "datacore" | */ ScriptLanguage;
-    script: string;
-    currentFile: string;
-    title: string;
+    /** Custom title for the view pane. */
+    title?: string;
+    /** Language that the script will be executed in. */
+    sourceType?: ScriptLanguage;
+    /** If defined, the file the view will be relative to. */
+    currentFile?: string;
+    /** Contents of the script. */
+    script?: string;
+    /** The current view; defaults to 'settings' for initialization. */
+    view?: "settings" | "script";
 }
 
 export class DatacoreQueryView extends ItemView {
+    /** Internal current state of the view; can be modified by setState. */
     public internalState: DatacoreViewState = {
-        title: "",
-        currentFile: "",
+        title: "New view",
         script: "",
         sourceType: "js",
+        view: "settings"
     };
-    public jsRenderer: DatacoreJSRenderer;
-    public settingsRenderer: ReactRenderer;
-    public isSettingsShowing: boolean;
-    private id: string;
-    navigation: boolean = false;
-    constructor(leaf: WorkspaceLeaf, public api: DatacoreApi, private plugin: DatacorePlugin) {
+
+    /** The current active view - either the settings view */
+    private activeView: MarkdownRenderChild;
+    private activeViewType: string;
+
+    constructor(leaf: WorkspaceLeaf, public api: DatacoreApi) {
         super(leaf);
         this.rerender();
     }
-    getViewType(): string {
-        return VIEW_TYPE_DATACORE;
-    }
-    getDisplayText(): string {
-        return `${this.internalState.title} (Datacore)`;
-    }
-    public async onload() {
-        this.settingsRenderer = new ReactRenderer(
-            this.app,
-            this.api.core,
-            this.contentEl,
-            this.internalState.currentFile || "",
-            (
-                <CUSTOM_VIEW_CONTEXT.Provider value={this}>
-                    <DatacoreViewSettings />
-                </CUSTOM_VIEW_CONTEXT.Provider>
-            )
-        );
 
-        this.id = this.leaf.serialize().id;
+    /** Should always be VIEW_TYPE_DATACOREJS. */
+    getViewType(): string {
+        return VIEW_TYPE_DATACOREJS;
+    }
+
+    /** Text shown in the title window. */
+    getDisplayText(): string {
+        return `${this.internalState.title} (DatacoreJS)`;
+    }
+
+    public async onload() {
         this.contentEl.addClass("markdown-rendered");
         this.rerender();
     }
-    public onPaneMenu(menu: Menu, source: "more-options" | "tab-header" | string): void {
-        if (source === "more-options") {
-            menu.addItem((it) => {
-                it.setIcon("settings");
-                it.setTitle("View configuration");
-                it.onClick((e) => {
-                    this.toggleSettings(true);
-                });
-            });
-        }
+
+    public onunload(): void {
+        if (this.activeView) this.removeChild(this.activeView);
     }
-    private settingsShowing: boolean;
-    public toggleSettings(show: boolean): void {
-        this.settingsShowing = show;
-        if (show) {
-            this.settingsRenderer = new ReactRenderer(
+
+    /** Synchronizes the screen state to properly reflect the current internal state. */
+    rerender(): void {
+        this.leaf.tabHeaderInnerTitleEl.textContent = this.titleEl.textContent = this.getDisplayText();
+        if (this.activeViewType == this.internalState.view) return;
+
+        if (this.activeView) this.removeChild(this.activeView);
+        if (this.internalState.view === "settings") {
+            this.activeViewType = "settings";
+            this.activeView = new ReactRenderer(
                 this.app,
                 this.api.core,
                 this.contentEl,
@@ -234,37 +199,61 @@ export class DatacoreQueryView extends ItemView {
                     </CUSTOM_VIEW_CONTEXT.Provider>
                 )
             );
-            this.removeChild(this.jsRenderer);
-            this.addChild(this.settingsRenderer);
         } else {
-            this.removeChild(this.settingsRenderer);
-            this.rerender();
+            // If a script, try to execute it; otherwise, show a reasonable error message.
+            this.activeViewType = "script";
+            if (this.internalState.script) {
+                this.activeView = new DatacoreJSRenderer(
+                    new DatacoreLocalApi(this.api, this.internalState.currentFile || ""),
+                    this.contentEl,
+                    this.internalState.currentFile || "",
+                    this.internalState.script || "",
+                    this.internalState.sourceType || "js"
+                );
+            } else {
+                this.activeView = new ReactRenderer(
+                    this.app,
+                    this.api.core,
+                    this.contentEl,
+                    this.internalState.currentFile || "",
+                    (
+                        <CUSTOM_VIEW_CONTEXT.Provider value={this}>
+                            <ErrorMessage message="No script defined for this view." />
+                        </CUSTOM_VIEW_CONTEXT.Provider>
+                    )
+                );
+            }
         }
+
+        this.addChild(this.activeView);
     }
+
     public getState() {
         return this.internalState;
-        // return this.internalState;
     }
+
+    /** Update the state of this view with new metadata. Generally controlled by the settings pane. */
+    public async setState(state: DatacoreViewState, _result: ViewStateResult): Promise<void> {
+        this.internalState = state;
+        this.rerender();
+    }
+
+    /** Swap the active view. */
+    public view(mode: "settings" | "script"): void {
+        this.internalState.view = mode;
+        this.rerender();
+    }
+
     public async onOpen(): Promise<void> {}
-    rerender(): void {
-				if(this.jsRenderer)
-	        this.removeChild(this.jsRenderer);
-        this.jsRenderer = new DatacoreJSRenderer(
-            new DatacoreLocalApi(this.api, this.internalState.currentFile || ""),
-            this.contentEl,
-            this.internalState.currentFile || "",
-            this.internalState.script || "",
-            this.internalState.sourceType || "js"
-        );
-        this.addChild(this.jsRenderer);
-    }
-    public async setState(state: DatacoreViewState, result: ViewStateResult): Promise<void> {
-        Object.assign(this.internalState, state);
-        this.leaf.tabHeaderInnerTitleEl.textContent = this.titleEl.textContent = this.getDisplayText();
-        if (!this.settingsShowing) this.rerender();
-    }
-    public onunload(): void {
-        this.removeChild(this.jsRenderer);
-        this.removeChild(this.settingsRenderer);
+
+    /** Handle for right click menus. */
+    public onPaneMenu(menu: Menu, source: "more-options" | "tab-header" | string): void {
+        if (source === "more-options") {
+            menu.addItem((it) => {
+                it.setIcon("settings");
+                it.setTitle("Configure View");
+                it.onClick((e) => this.view("settings"));
+            });
+        }
     }
 }
