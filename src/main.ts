03fcdcce741b5cdaf7190d691d11aa7ed62d6ace
Michael Brenan <michael@brenan.dev>
2024-10-07 22:51:29 -0700

    Refactor and simplify a bit of logic

--- src/main.ts
+++ src/main.ts
@@ -77,7 +77,6 @@ export default class DatacorePlugin extends Plugin {
             this.core.initialize();
         }
 
-
         // Make the API globally accessible from any context.
         window.datacore = this.api;
 
