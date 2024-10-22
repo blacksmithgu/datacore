8c0b5245f77864b21cd20459034ed501875ac980
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <i.am.the.tablet@proton.me>
2024-10-21 22:44:48 -0400

    re-add 1 to `list.position.end.line` in markdown import

--- src/index/import/markdown.ts
+++ src/index/import/markdown.ts
@@ -173,7 +173,7 @@ export function markdownSourceImport(
 
         const item = new ListItemData(
             list.position.start.line,
-            list.position.end.line,
+            list.position.end.line + 1,
             list.parent,
             symbol,
             list.id,
