28ebd1a57afaaa1b28570a81d4b00efd4e6dc08e
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <i.am.the.tablet@proton.me>
2024-11-01 18:10:42 -0400

    fix for multiline list items

--- src/index/import/markdown.ts
+++ src/index/import/markdown.ts
@@ -31,7 +31,7 @@ const YAML_DATA_REGEX = /```yaml:data/i;
 /** Matches the start of any codeblock fence. */
 const CODEBLOCK_FENCE_REGEX = /^(?:```|~~~)(.*)$/im;
 /** Matches list items (including inside text blocks). */
-const LIST_ITEM_REGEX = /^[\s>]*(\d+\.|\d+\)|\*|-|\+)\s*(\[.{0,1}\])?\s*(.*)$/mu;
+const LIST_ITEM_REGEX = /^[\s>]*(\d+\.|\d+\)|\*|-|\+)\s*(\[.{0,1}\])?\s*(.*)/msu;
 
 /**
  * Given the raw source and Obsidian metadata for a given markdown file,
@@ -160,7 +160,7 @@ export function markdownSourceImport(
     const listItems = new BTree<number, ListItemData>(undefined, (a, b) => a - b);
 
     for (const list of metadata.listItems || []) {
-        const line = lines[list.position.start.line];
+        const line = lines.slice(list.position.start.line, list.position.end.line + 1).join("\n");
 
         // TODO: Implement flag which skips indexing list items.
         const match = line.match(LIST_ITEM_REGEX);
