e6d213e6a891e46e7e6df0fc61c620537ce901a6
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <i.am.the.tablet@proton.me>
2025-04-19 19:00:08 -0400

    add tree table to local api

--- src/api/local-api.tsx
+++ src/api/local-api.tsx
@@ -40,6 +40,7 @@ import {
     FieldSlider,
     FieldSwitch,
 } from "ui/fields/editable-fields";
+import { TreeTableView } from "./ui/views/tree-table";
 
 /**
  * Local API provided to specific codeblocks when they are executing.
@@ -473,6 +474,7 @@ export class DatacoreLocalApi {
     /** A single card which can be composed into a grid view. */
     public Card = Card;
     public TaskList = TaskList;
+		public TreeTable = TreeTableView;
 
     /////////////////////////
     // Interative elements //
