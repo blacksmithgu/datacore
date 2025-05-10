045aecc80a9f1929947490a9ffec3b0ad8294bae
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <i.am.the.tablet@proton.me>
2025-05-10 01:29:20 -0400

    move select augmentation to proper folder, re-export some things

--- src/typings/select.d.ts
+++ src/typings/select.d.ts
@@ -4,6 +4,7 @@ declare module "react-select" {
 		import { StateManagerAdditionalProps } from "react-select/dist/declarations/src/useStateManager";
 		import { Props } from "react-select/dist/declarations/src/Select";
 		import Select from "react-select/dist/declarations/src/Select";
+		export * from "react-select/dist/declarations/src/types";
 		declare type StateManagedPropKeys = 'inputValue' | 'menuIsOpen' | 'onChange' | 'onInputChange' | 'onMenuClose' | 'onMenuOpen' | 'value';
 		declare type PublicBaseSelectProps<Option, IsMulti extends boolean, Group extends GroupBase<Option>> = JSX.LibraryManagedAttributes<typeof Select, Props<Option, IsMulti, Group>>;
 		declare type SelectPropsWithOptionalStateManagedProps<Option, IsMulti extends boolean, Group extends GroupBase<Option>> = Omit<PublicBaseSelectProps<Option, IsMulti, Group>, StateManagedPropKeys> & Partial<PublicBaseSelectProps<Option, IsMulti, Group>>;
