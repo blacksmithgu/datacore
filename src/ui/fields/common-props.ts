8d79d54607e98ebf348f9f52dd6ade3004acc20b
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <i.am.the.tablet@proton.me>
2025-08-03 21:22:42 -0400

    re-add `src/ui/fields` folder (for now?)

---
+++ src/ui/fields/common-props.ts
@@ -0,0 +1,29 @@
+/**
+ * @module ui
+ */
+import { LiteralType } from "expression/literal";
+import { InlineField } from "index/import/inline-field";
+
+/** Props for any kind of editable field.
+ * @group Props
+ * @typeParam T - the type of field being edited
+ */
+export interface FieldControlProps<T> extends BaseFieldProps<T> {
+    field: InlineField;
+    value: T;
+    file: string;
+    updater?: (val: T) => unknown;
+}
+
+/**
+ * @group Props
+ *
+ */
+
+export interface BaseFieldProps<T> {
+    type: LiteralType;
+    defaultValue?: T | (() => T);
+    renderAs?: "progress" | "rating" | "select" | "raw";
+    // TODO: type this better
+    config?: Record<string, any>;
+}
