83b5edb7ad23c26dc4ee9b55836f1ab3fe3e9d32
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <i.am.the.tablet@proton.me>
2026-02-11 14:26:51 -0500

    add missing `is-collapsed` class to callout's div when closed

--- src/api/ui/views/callout.tsx
+++ src/api/ui/views/callout.tsx
@@ -53,7 +53,7 @@ export function Callout({
             data-callout-metadata={type?.split(METADATA_SPLIT_REGEX)?.[1]}
             data-callout={type?.split(METADATA_SPLIT_REGEX)?.[0]}
             data-callout-fold={open ? "+" : "-"}
-            className={combineClasses("datacore", "callout", collapsible ? "is-collapsible" : undefined)}
+            className={combineClasses("datacore", "callout", collapsible ? "is-collapsible" : undefined, !open ? "is-collapsed" : undefined)}
         >
             <div className="callout-title" onClick={() => collapsible && setOpen(!open)}>
                 {icon && <div className="callout-icon">{icon}</div>}
