437e44c90a67feeb77431025952a144545b97288
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <i.am.the.tablet@proton.me>
2026-03-17 01:25:52 -0400

    add setup scripts

---
+++ src/test/e2e/__setup.ts
@@ -0,0 +1,1 @@
+export {default as default} from "obsidian-testing-framework/setup";
--- vitest.config.ts
+++ vitest.config.ts
@@ -4,13 +4,17 @@ import {defineConfig} from "vitest/config";
 export default defineConfig({
 	test: {
 		dir: "src/test/e2e/",
-		pool: "threads",
+		pool: "forks",
+		globalSetup: ["./src/test/e2e/__setup.ts"],
 		provide: {
 			vault: path.resolve(process.cwd(), "test-vault")
 		},
 		hookTimeout: 60000,
 		retry: 5,
-		fileParallelism: false,
+		fileParallelism: true,
+		maxWorkers: 2,
+		isolate: false,
+
 		exclude: [],
 		testTimeout: 120000,
 		watch: !process.env.CI,
